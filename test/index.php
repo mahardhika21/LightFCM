d60df25e0ee15a2f5944c3e2506413e346fd3a1b
mahardhika21 <mahardhika894@gmail.com>
2020-09-08 22:01:29 +0700

    up

--- test/index.php
+++ test/index.php
@@ -10,7 +10,7 @@ class Test
 	public function send()
 	{
 		// create the client
-		 $light = new LightFcm("AAAA5-dUwP0:APA91bHKlvae9pp1icUe6ApIQwY4-tsHVjnAPE1umBWq3yEbcOPzCP64cZdrXGnPW37m5Gc0ye54NwNWOykCiWFN3cTzX0Z2g0Wo9-ECSt0aVjQj_03O5fXn9JVz4rxFkL98ytwusvBR");
+		 $light = new LightFcm("AAAA5-dUwP0:server-key*********************************");
 
 		 $message = [
 		 	"title"   => "title notif",
@@ -21,7 +21,7 @@ class Test
 
 		// register send tokens
 		$tokens = [
-			"fNw6Dfe1S6KZJzCDp8Vgmo:APA91bGIFz6uC26GsaL_5DHEJmuAvw5igm0E8Tz2lTmWT5LrTKMBrYmUgO709k4uRYMxa1LY6tqCgAxAwG8eZDJRKrJC-pgGYsnp88s_HYu87xQR2cH8Bcg4ZxUdIQiMLOoIlLJ1I6hN",
+			"fNw6Dfe1S6KZJzCDp8Vgmo:client token *******************************************",
 		];
 		// and send
 		return $light->sendFcm($message, $tokens);
